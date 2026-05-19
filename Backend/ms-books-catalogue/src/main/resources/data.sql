319721e0d3d0af9a7f10453caffc80778b5285ee
camilohzm04-beep <camilohzm04@gmail.com>
2026-05-19 15:54:59 -0500

    camilohzm04-beep-patch-1

---
+++ Backend/ms-books-catalogue/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (title, author, publication_date, category, isbn, rating, visible, stock)
+VALUES
+  ('El Coronel no tiene quien le escriba', 'Gabriel Garcia Marquez', '1961-01-01', 'Novela', '978849759235', 5, TRUE, 12),
+  ('Habitos atomicos', 'James Clear', '2018-10-16', 'Desarrollo personal', '9780735211292', 5, TRUE, 20),
+  ('Padre Rico, Padre Pobre', 'Robert T. Kiyosaki', '1997-04-01', 'Finanzas', '9788466332125', 4, TRUE, 15),
+  ('Rompe La Barrera Del No', 'Chris Voss con Tahl Raz', '2016-05-17', 'Negociacion', '9789588821474', 5, TRUE, 10),
+  ('Construir Confianza', 'Ricardo Sala', '2024-01-01', 'Empresa', '9786280189437', 4, TRUE, 9),
+  ('Programacion Para Principiantes', 'Alexander Cane', '2019-10-09', 'Tecnologia', '9781699052327', 4, TRUE, 18),
+  ('Emprendimiento Digital', 'Jose Antonio Rojas, Javier Garcia Blasques y Rosa Moore Torres', '2023-01-01', 'Empresa', '9789587928068', 4, TRUE, 8),
+  ('Productividad Total', 'David Medianero Burga', '2016-01-01', 'Productividad', '9786123044152', 4, TRUE, 11);
+
